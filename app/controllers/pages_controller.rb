53308ec7984c55f36031a0547a6450ee6f71ff07
Emil Ingemansson <emilbingemansson@gmail.com>
2020-10-05 20:18:23 +0000

    Added an extra page so users can select which type of package they want to sign up for after clicking sign up button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,19 @@
 class PagesController < ApplicationController
+  #GET Request for /
   def home
   end
   
+   #GET Request for /about
   def about
   end
   
+   #GET Request for /faqs
   def faqs
   end
   
+   #GET Request for /memberships
   def memberships
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
 end
