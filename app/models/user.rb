19464a0e8de3702638a4e0438216e45db27b26dc
Emil Ingemansson <emilbingemansson@gmail.com>
2020-10-20 23:40:50 +0000

    Added comments to files for Stripe functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   belongs_to :plan
   
   attr_accessor :stripe_card_token
+  # If Pro user passes the validation. Send card information to Stripe
+  # to create a pro plan subscription and use the 
+  # use the incoming card token from Stripe to save to the database. 
+  # So we do not save any card information. Stripe handles all of that. 
   def save_with_subscription
     if valid?
       customer = Stripe::Customer.create(description: email, card: stripe_card_token)
